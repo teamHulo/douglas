d0776fd61d821f5a267de968378930df74a3af5a
Aleksey <alenrouse123@gmail.com>
2023-09-21 15:58:36 +0300

    update 21.09.2023 product page and style cart login...

--- assets/a-script.js
+++ assets/a-script.js
@@ -30,3 +30,61 @@ $(() => {
       .addClass("active");
   });
 });
+$(() => {
+  console.clear();
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  const panels = gsap.utils.toArray(".panel");
+  const contentoEls = gsap.utils.toArray(".contento");
+
+  const toggleReveal = (index) => {
+    const next = contentoEls[index];
+    const prev = contentoEls[index - 1];
+    next && next.classList.toggle("revealed");
+    prev && prev.classList.toggle("revealed");
+  };
+
+  gsap.set(panels, {
+    yPercent: (i) => (i ? 100 : 0),
+    opacity: (i) => (i ? 0 : 1) // Устанавливаем начальную нулевую непрозрачность для всех, кроме первой панели
+  });
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".section-scroll-slides",
+      start: "top top",
+      end: () => "+=" + 100 * panels.length + "%",
+      pin: true,
+      scrub: 1
+    }
+  });
+
+  panels.forEach((panel, index) => {
+    if (index) {
+      tl.to(
+        panels[index - 1], // Предыдущая панель
+        {
+          opacity: 0, // Скрываем предыдущую панель (fade out)
+          yPercent: -100,
+          ease: "none"
+        },
+        "+=0.15" // Задержка перед следующей анимацией
+      );
+      tl.to(
+        panel, // Текущая панель
+        {
+          opacity: 1, // Делаем текущую панель видимой (fade in)
+          yPercent: 0,
+          ease: "none"
+        },
+        "-=0.15" // Перекрываем предыдущую анимацию (fade out)
+      );
+      if (contentoEls[index]) {
+        tl.call(toggleReveal, [index], "<+=0.125");
+      }
+    } else {
+      tl.call(toggleReveal, [index], 0.125);
+    }
+  });
+});
